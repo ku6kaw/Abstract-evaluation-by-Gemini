b73fcd8e128bae79c8ddf56b6db12b767287e2fc
kurokawa <reoreo01.123@gmail.com>
2025-01-06 22:29:00 +0900

    update rule dependencies

--- src/analysis/functions.py
+++ src/analysis/functions.py
@@ -8,25 +8,33 @@ def process_csv_with_dependencies(input_path, output_path):
         output_path (str): 更新されたCSVファイルを保存するパス。
     """
     
-    # ルールの依存関係を定義
-    dependencies = {
-        'rule1': ['rule2', 'rule3', 'rule4'],  # rule1に依存するルール
-        'rule5': ['rule6', 'rule7', 'rule8', 'rule9', 'rule10'],  # rule5に依存するルール
-        'rule6': ['rule7'],  # 追加：rule6がyesの場合、rule7もyesにする
-        'rule11': ['rule12'],  # rule11に依存するルール
-        'rule13': ['rule15'],  # rule13に依存するルール
-        'rule17': ['rule18', 'rule19', 'rule20', 'rule21', 'rule22', 'rule23', 'rule24', 'rule27'],  # rule17に依存するルール
-        'rule25': ['rule26', 'rule28'],  # rule26に依存するルール
+    # noのときに依存するルール
+    no_dependencies = {
+        'rule1': ['rule2', 'rule3', 'rule4'],
+        'rule5': ['rule6', 'rule7', 'rule8', 'rule9', 'rule10'],
+        'rule11': ['rule12'],
+        'rule13': ['rule15'],
+        'rule17': ['rule18', 'rule19', 'rule20', 'rule21', 'rule22', 'rule23', 'rule24', 'rule27'],
+        'rule25': ['rule26', 'rule28'],
+    }
+    
+    # yesのときに依存するルール
+    yes_dependencies = {
+        'rule6': ['rule7'],  # 特別: rule6がyesの場合、rule7もyesにする
     }
     
     # CSVファイルの読み込み
     df = pd.read_csv(input_path)
     
-    # 依存関係に基づいて値を変更
-    for key_rule, dependent_rules in dependencies.items():
+    # noの依存関係に基づいて値を変更
+    for key_rule, dependent_rules in no_dependencies.items():
         for dependent_rule in dependent_rules:
             # key_ruleが"no"の場合、dependent_rulesをすべて"no"に変更
             df.loc[df[key_rule] == 'no', dependent_rule] = 'no'
+    
+    # yesの依存関係に基づいて値を変更
+    for key_rule, dependent_rules in yes_dependencies.items():
+        for dependent_rule in dependent_rules:
             # key_ruleが"yes"の場合、dependent_rulesをすべて"yes"に変更
             df.loc[df[key_rule] == 'yes', dependent_rule] = 'yes'
     
